4edc41b0276b426eb900f10d78fe6b70944d38e8
Dipti saha <sahadipti322@gmail.com>
2022-10-27 10:44:09 -0400

    footer add

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-light py-2'>
+            <p className='text-center'>Copyright.All Rights Reserved</p>
+        </div>
+    );
+};
+
+export default Footer;
